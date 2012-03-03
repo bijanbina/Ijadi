aa98b530b8fb1b5f757f8f969a47747fa9964e6f
Bijan Binaee <bijanbina@gmail.com>
2012-03-03 05:45:27 +0330

    Add ijadi-define.h

---
+++ Source/ijadi-define.h
@@ -0,0 +1,24 @@
+/* --------------------------------------- Besm Allah rahman rahim   ----------------------------------------------------
+ *
+ * Copyright (C) 2011 AllahSoft Std.
+ *
+ * Bijan Binaee   <bijanbina@gmail.com>             
+ * Reza Faramarzi <faramarzi.reza14@gmail.com>
+ *
+*  ijadi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Info: this file is for creating main window of the ijadi project
+ */
+
+#ifndef _IJADI_DEFINE_H_
+#define _IJADI_DEFINE_H_
+
+#include <config.h>
+
+#define LOCAL_RESOURCES IJADI_RESOURCES
+//#define LOCAL_RESOURCES "Resources"
+
+#endif /* _IJADI_GUI_H_ */
